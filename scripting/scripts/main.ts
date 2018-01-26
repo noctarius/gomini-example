e259d7b0fadb429e69061fc9c51e328bc6a77cde
noctarius <me@noctarius.com>
2018-01-26 16:49:12 +0100

    Little bit more console output

--- scripting/scripts/main.ts
+++ scripting/scripts/main.ts
@@ -6,12 +6,15 @@ http.registerRequestHandler("/foo", http.RequestMethod.GET, (context) => {
     return context.response.respondWithString(http.ResponseCode.OK, "BAM!")
 });
 
+console.log("main.ts: Initializing /foo/bar handler...");
 http.registerRequestHandler("/foo/bar", http.RequestMethod.GET, (context) => {
     return context.response.respondWithError(http.ResponseCode.NotFound)
 });
 
+console.log("main.ts: Initializing /error handler...");
 http.registerRequestHandler("/error", http.RequestMethod.GET, () => {
     foo();
+    return null;
 });
 
 const foo = () => {
